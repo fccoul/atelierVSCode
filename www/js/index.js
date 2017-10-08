f53a345c78b284198636199e1d2a20801e093200
fccoul <fccoul@gmail.com>
2017-10-08 20:24:17 +0000

    ok cmd palette

--- www/js/index.js
+++ www/js/index.js
@@ -16,6 +16,7 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+var _val="ok msg !";
 var app = {
     // Application Constructor
     initialize: function() {
